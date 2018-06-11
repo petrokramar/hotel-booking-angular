5229e642b97fac7b40b2f62d418370322f42488d
starikivan <peter.v.kramar@gmail.com>
2018-06-11 19:17:02 +0300

    Finding-room fix change checkIn and checkOut

--- src/app/components/finding-room/finding-room.component.ts
+++ src/app/components/finding-room/finding-room.component.ts
@@ -17,6 +17,10 @@ export class FindingRoomComponent implements OnInit {
 
   filteredCities: City[] = [];
   cityCtrl: FormControl;
+  checkInDate: Date = new Date();
+  checkOutDate: Date = new Date();
+  minCheckInDate: Date = new Date();
+  minCheckOutDate: Date = new Date();
   isLoaded = false;
 
   constructor(
@@ -30,7 +34,11 @@ export class FindingRoomComponent implements OnInit {
   }
 
   ngOnInit() {
-   }
+    this.setDate(this.minCheckInDate, this.minCheckInDate, 1);
+    this.setDate(this.minCheckOutDate, this.minCheckInDate, 1);
+    this.setDate(this.checkInDate, this.minCheckInDate, 0);
+    this.setDate(this.checkOutDate, this.minCheckOutDate, 0);
+  }
 
   displayCity(city): string {
     return city ? city.name : city;
@@ -47,7 +55,21 @@ export class FindingRoomComponent implements OnInit {
       });
   }
 
+  checkDates(): void {
+    this.setDate(this.minCheckOutDate, this.checkInDate, 1);
+    if (this.checkInDate >= this.checkOutDate) {
+      this.checkOutDate = new Date();
+      this.setDate(this.checkOutDate, this.minCheckOutDate, 0);
+    };
+  }
+
+  setDate(targetDate: Date, baseDate: Date, addDays: number): void {
+    targetDate.setFullYear(baseDate.getFullYear(), baseDate.getMonth(), baseDate.getDate() + addDays);
+  };
+
   findHotels(): void {
+    console.log(this.checkInDate);
+    console.log(this.checkOutDate);
     console.log(this.cityCtrl.value.id);
   }
 }
